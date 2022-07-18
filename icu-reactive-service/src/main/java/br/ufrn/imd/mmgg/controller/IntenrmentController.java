fb3606f0e6acaf8c4486180535ee91948a993763
matheusmota3242 <matheusmota3242@gmail.com>
2022-07-17 22:17:38 -0300

    implementando serviço gateway

--- icu-reactive-service/src/main/java/br/ufrn/imd/mmgg/controller/IntenrmentController.java
+++ icu-reactive-service/src/main/java/br/ufrn/imd/mmgg/controller/IntenrmentController.java
@@ -5,6 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ufrn.imd.mmgg.model.ICUInterment;
@@ -12,6 +13,7 @@ import br.ufrn.imd.mmgg.service.InternmentService;
 import reactor.core.publisher.Mono;
 
 @RestController
+@RequestMapping("/icu")
 public class IntenrmentController {
     
     @Autowired
